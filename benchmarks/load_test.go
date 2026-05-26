e106516a5904ff4944eb61c32b1b6b7cc7d07329
tanmayjoddar <tanmayjoddar17@gmail.com>
2026-05-26 14:00:00 +0530

    Add benchmark suite with proof reduction, concurrency, warm read, and cache instrumentation

---
+++ benchmarks/load_test.go
@@ -0,0 +1,69 @@
+package benchmarks
+
+import (
+	"fmt"
+	"testing"
+	"wavicle/internal/core"
+	"wavicle/internal/engine"
+	"wavicle/internal/storage"
+)
+
+func BenchmarkConcurrency_1000Clients(b *testing.B) {
+	crystal, _ := storage.NewCausalCrystal("bench_load.log")
+
+	for i := 0; i < 100; i++ {
+		crystal.AppendAtom(&core.EConst{Value: core.VString("val")}, fmt.Sprintf("key_%d", i), nil)
+	}
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			key := fmt.Sprintf("key_%d", i%100)
+			if i%10 == 0 {
+				crystal.AppendAtom(&core.EConst{Value: core.VString("new_val")}, key, nil)
+			} else {
+				atom, _ := crystal.GetCurrent(key)
+				if atom != nil {
+					proof := &engine.MaterializedProof{
+						ProofTree:     atom.Expr,
+						VersionVector: &engine.VersionVector{Entries: map[string]core.Hash{key: atom.Hash}},
+						NodeCache:     make(map[core.Hash]core.Value),
+						Value:         atom.Expr.(*core.EConst).Value,
+					}
+					engine.ReduceIncremental(proof, crystal)
+				}
+			}
+			i++
+		}
+	})
+}
+
+func BenchmarkWarmRead_Wavicle_vs_RedisSim(b *testing.B) {
+	crystal, _ := storage.NewCausalCrystal("bench_warm.log")
+	key := "warm_key"
+	h, _ := crystal.AppendAtom(&core.EConst{Value: core.VString("warm_val")}, key, nil)
+	atom, _ := crystal.GetCurrent(key)
+
+	proof := &engine.MaterializedProof{
+		ProofTree:     atom.Expr,
+		Value:         atom.Expr.(*core.EConst).Value,
+		VersionVector: &engine.VersionVector{Entries: map[string]core.Hash{key: h}},
+		NodeCache:     make(map[core.Hash]core.Value),
+	}
+
+	b.Run("Wavicle_WarmRead", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, _ = engine.ReduceIncremental(proof, crystal)
+		}
+	})
+
+	redisSim := make(map[string]string)
+	redisSim[key] = "warm_val"
+
+	b.Run("RedisSim_EngineOnly", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = redisSim[key]
+		}
+	})
+}
---
+++ benchmarks/proof_bench_test.go
@@ -0,0 +1,155 @@
+package benchmarks
+
+import (
+	"fmt"
+	"testing"
+	"wavicle/internal/core"
+	"wavicle/internal/engine"
+	"wavicle/internal/storage"
+)
+
+func setup50FieldCrystal(b *testing.B) (*storage.CausalCrystal, *core.ECompose, map[string]core.Hash, []core.Hash) {
+	crystal, err := storage.NewCausalCrystal("bench_crystal.log")
+	if err != nil {
+		b.Fatal(err)
+	}
+	var atoms []core.Hash
+	entries := make(map[string]core.Hash)
+	for i := 0; i < 50; i++ {
+		path := fmt.Sprintf("user:123:field_%d", i)
+		h, err := crystal.AppendAtom(
+			&core.EConst{Value: core.VString(fmt.Sprintf("value_%d", i))},
+			path, nil,
+		)
+		if err != nil {
+			b.Fatal(err)
+		}
+		atoms = append(atoms, h)
+		entries[path] = h
+	}
+	expr := &core.ECompose{Atoms: atoms}
+	return crystal, expr, entries, atoms
+}
+
+func BenchmarkProofReduction_Incremental_Warm(b *testing.B) {
+	crystal, expr, entries, atoms := setup50FieldCrystal(b)
+
+	proof := &engine.MaterializedProof{
+		ProofTree:     expr,
+		VersionVector: &engine.VersionVector{Entries: entries},
+		NodeCache:     make(map[core.Hash]core.Value),
+		PathToExpr:    make(map[string]core.Hash),
+	}
+	for i, atomHash := range atoms {
+		proof.PathToExpr[fmt.Sprintf("user:123:field_%d", i)] = atomHash
+	}
+
+	warmVal, warmCache, err := engine.ReduceProofTree(expr, crystal)
+	if err != nil {
+		b.Fatal(err)
+	}
+	proof.NodeCache = warmCache
+	proof.Value = warmVal
+
+	if len(proof.NodeCache) < 50 {
+		b.Fatalf("node cache has %d entries, expected >= 50", len(proof.NodeCache))
+	}
+
+	crystal.AppendAtom(
+		&core.EConst{Value: core.VString("new_value")},
+		"user:123:field_0", nil,
+	)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = engine.ReduceIncremental(proof, crystal)
+	}
+
+	b.Logf("cache_hits=%d cache_misses=%d changed_paths=%d",
+		proof.ReduceStats.CacheHits,
+		proof.ReduceStats.CacheMisses,
+		proof.ReduceStats.ChangedPaths,
+	)
+}
+
+func BenchmarkProofReduction_WarmReuse_FastPath1(b *testing.B) {
+	crystal, expr, entries, atoms := setup50FieldCrystal(b)
+
+	snapshot := make(map[string]core.Hash)
+	for k, v := range entries {
+		snapshot[k] = v
+	}
+
+	proof := &engine.MaterializedProof{
+		ProofTree:     expr,
+		VersionVector: &engine.VersionVector{Entries: snapshot},
+		NodeCache:     make(map[core.Hash]core.Value),
+		PathToExpr:    make(map[string]core.Hash),
+	}
+	for i, atomHash := range atoms {
+		proof.PathToExpr[fmt.Sprintf("user:123:field_%d", i)] = atomHash
+	}
+
+	warmVal, warmCache, _ := engine.ReduceProofTree(expr, crystal)
+	proof.NodeCache = warmCache
+	proof.Value = warmVal
+
+	engine.ReduceIncremental(proof, crystal)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = engine.ReduceIncremental(proof, crystal)
+	}
+}
+
+func BenchmarkProofReduction_FastPath2_MerkleMatch(b *testing.B) {
+	crystal, err := storage.NewCausalCrystal("bench_merkle.log")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	var atoms []core.Hash
+	var paths []string
+	entries := make(map[string]core.Hash)
+	for i := 0; i < 10; i++ {
+		path := fmt.Sprintf("path_%d", i)
+		h, _ := crystal.AppendAtom(&core.EConst{Value: core.VString("val")}, path, nil)
+		atoms = append(atoms, h)
+		paths = append(paths, path)
+		entries[path] = h
+	}
+
+	expr := &core.ECompose{Atoms: atoms}
+	proof := &engine.MaterializedProof{
+		ProofTree:     expr,
+		VersionVector: &engine.VersionVector{Entries: entries},
+		NodeCache:     make(map[core.Hash]core.Value),
+	}
+
+	engine.ReduceIncremental(proof, crystal)
+	proof.MerkleRoot = crystal.MerkleRootForPaths(paths)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = engine.ReduceIncremental(proof, crystal)
+	}
+}
+
+func BenchmarkProofReduction_Cold(b *testing.B) {
+	crystal, expr, _, atoms := setup50FieldCrystal(b)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		nodeCache := make(map[core.Hash]core.Value)
+		proof := &engine.MaterializedProof{
+			ProofTree:     expr,
+			VersionVector: &engine.VersionVector{Entries: map[string]core.Hash{"invalid": {}}},
+			NodeCache:     nodeCache,
+			PathToExpr:    map[string]core.Hash{},
+		}
+		for j, atomHash := range atoms {
+			proof.PathToExpr[fmt.Sprintf("user:123:field_%d", j)] = atomHash
+		}
+		_, _ = engine.ReduceIncremental(proof, crystal)
+	}
+}
